df674dc7fa04c020f95b8f176b59f3aca289d3aa
magerishere <immagerishere@gmail.com>
2021-05-08 05:24:19 -0700

    message tab almost

--- resources/views/backend/messages/index.blade.php
+++ resources/views/backend/messages/index.blade.php
@@ -16,36 +16,56 @@
                     </div>
 
                     <div class="col-md-10">
-                        <div class="card">
+                        <div class="card list-messages">
                             <div class="card-header"></div>
                             <div class="card-body">
                                 <ul class="list-group">
                                     <li class="list-group-item">
-                                        <div class="form-check">
-                                        
+                                        <div>
+                                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                                            <img src="{{ asset('assets/images/avatar.png') }}" alt="avatar"
+                                                class="rounded-pill d-inline-block message-sender-image">
+                                            <h5 class="d-inline-block message-sender">علی خوشکار </h5>
 
-                                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                                                <label class="form-check-label" for="flexCheckDefault">
-                                                    سلام خوبی
-                                                </label>
-                                            
+                                            <span class="message-body">
+                                                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از
+                                                چاپگرها ...
+                                            </span>
+
 
                                             <div class="float-end">
-
-                                                <ul class="list-group">
-
-                                                    <li class="list-group-item">
-                                                        <i id="star" class="fa fa-star-o mx-2" aria-hidden="true"></i>
-                                                        <i class="fa fa-trash mx-2" aria-hidden="true"></i>
-                                                    </li>
-                                                </ul>
+                                                <i class="fa fa-star-o mx-2 star" aria-hidden="true"></i>
+                                                <i class="fa fa-trash mx-2" aria-hidden="true"></i>
                                             </div>
-
                                         </div>
+
+                                    </li>
+
+                                    <li class="list-group-item">
+                                        <div>
+                                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                                            <img src="{{ asset('assets/images/avatar.png') }}" alt="avatar"
+                                                class="rounded-pill d-inline-block message-sender-image">
+                                            <h5 class="d-inline-block message-sender">محمود </h5>
+
+                                            <span class="message-body">
+                                                امید داشت که تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان
+                                       
+                                            </span>
+
+
+                                            <div class="float-end">
+                                                <i id="star" class="fa fa-star-o mx-2 star" aria-hidden="true"></i>
+                                                <i class="fa fa-trash mx-2" aria-hidden="true"></i>
+                                            </div>
+                                        </div>
+
                                     </li>
                                 </ul>
                             </div>
                         </div>
+
+                        @include('backend.messages.layouts.message')
                     </div>
                 </div>
             </div>
@@ -56,9 +76,33 @@
 
 @section('footer')
     <script>
-        $('#star').click(function() {
+        $('.star').click(function(e) {
+            e.stopPropagation();
             $(this).toggleClass('fa-star-o');
             $(this).toggleClass('fa-star');
+        });
+
+        $('input[type="checkbox"]').click(function(e) {
+            e.stopPropagation();
+            $(this).parent().parent().toggleClass('checked');
+        });
+
+        $('.list-group-item').click(function(e) {
+            $('.list-messages').hide();
+            $('#message').show();
+            const sender = $(this).find('.message-sender').text();
+            const body = $(this).find('.message-body').text();
+            const image = $(this).find('.message-sender-image').attr('src');
+            console.log(image);
+            $('#message').find('#message-sender').text(sender)
+            $('#message').find('#message-sender-image').attr('src', image)
+            $('#message').find('#message-body').text(body)
+
+        });
+
+        $('.fa-arrow-left').click(function() {
+            $('#message').hide();
+            $('.list-messages').show();
         });
 
     </script>
---
+++ resources/views/backend/messages/layouts/message.blade.php
@@ -0,0 +1,34 @@
+<div class="card" id="message">
+    <div class="card-header text-center">
+        <div>
+
+            <h5 class="d-inline-block" id="message-sender"></h5>
+            <img src="" id="message-sender-image" class="d-inline-block rounded-pill" alt="">
+            <i class="fa fa-arrow-left float-end" aria-hidden="true"></i>
+        </div>
+    </div>
+    <div class="card-body">
+        <p id="message-body"></p>
+
+        <p> پیوست ها:
+        <div class="badge bg-primary text-wrap">
+            لینک 1
+        </div>
+        <div class="badge bg-primary text-wrap">
+            لینک 2
+        </div>
+        </p>
+
+
+        <form action="">
+            <div class="mb-3">
+
+                <textarea name="message" class="form-control" cols="30" rows="10"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">ارسال <i class="fa fa-paper-plane"
+                    aria-hidden="true"></i></button>
+            <button type="button" class="btn btn-secondary">انصراف <i class="fa fa-window-close"
+                    aria-hidden="true"></i></button>
+        </form>
+    </div>
+</div>
